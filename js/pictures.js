dbd0e0de077f04537e59cbf098aa7023de8a9944
Yerzhan <yerzhankab@gmail.com>
2026-08-31 01:13:37 +0500

    Добавил функци отрисовки фото и добавил в DOM

---
+++ js/pictures.js
@@ -0,0 +1,22 @@
+const picturesContainer = document.querySelector('.pictures');
+const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+
+const renderPictures = (pictures) => {
+  const picturesFragment = document.createDocumentFragment();
+
+  pictures.forEach(({url, description, likes, comments}) => {
+    const pictureElement = pictureTemplate.cloneNode(true);
+    const pictureImage = pictureElement.querySelector('.picture__img');
+
+    pictureImage.src = url;
+    pictureImage.alt = description;
+    pictureElement.querySelector('.picture__likes').textContent = likes;
+    pictureElement.querySelector('.picture__comments').textContent = comments.length;
+
+    picturesFragment.append(pictureElement);
+  });
+
+  picturesContainer.append(picturesFragment);
+};
+
+export {renderPictures};
