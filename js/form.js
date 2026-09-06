22243f5bf8faf3aeb73edf190da309dac2ed3b3c
Yerzhan <yerzhankab@gmail.com>
2026-09-07 03:57:13 +0500

    добавляет фильтры на превью фото

--- js/form.js
+++ js/form.js
@@ -1,3 +1,5 @@
+import {resetScale} from './scale.js';
+import {resetEffects} from './effects.js';
 // Получаем элементы формы загрузки изображения
 const uploadForm = document.querySelector('.img-upload__form');
 const uploadFileInput = uploadForm.querySelector('.img-upload__input');
@@ -86,6 +88,8 @@ function closeUploadForm() {
   uploadOverlay.classList.add('hidden');
   document.body.classList.remove('modal-open');
   uploadForm.reset();
+  resetScale();
+  resetEffects();
   document.removeEventListener('keydown', onDocumentKeydown);
   // Очистить ошибки валидации и сбросить классы CSS
   pristine.reset();
