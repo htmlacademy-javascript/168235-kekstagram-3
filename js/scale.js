80a98ecae7f78c43109c177fbb1cea3d7b262bd6
Yerzhan <yerzhankab@gmail.com>
2026-09-08 01:16:45 +0500

    перенёс в функцию повторящийся код в scale.js

--- js/scale.js
+++ js/scale.js
@@ -10,13 +10,16 @@ const scaleControlBigger = document.querySelector('.scale__control--bigger');
 const scaleControlValue = document.querySelector('.scale__control--value');
 // Изображение, к которому применяется масштаб
 const imgUploadPreview = document.querySelector('.img-upload__preview img');
+const applyScale = (scale) => {
+  scaleControlValue.value = `${scale}%`;
+  imgUploadPreview.style.transform = `scale(${scale / 100})`;
+};
 
 scaleControlSmaller.addEventListener('click', () => {
   let currentScale = parseInt(scaleControlValue.value, 10);
   if (currentScale > MIN_SCALE) {
     currentScale -= SCALE_STEP;
-    scaleControlValue.value = `${currentScale}%`;
-    imgUploadPreview.style.transform = `scale(${currentScale / 100})`;
+    applyScale(currentScale);
   }
 });
 
@@ -24,15 +27,13 @@ scaleControlBigger.addEventListener('click', () => {
   let currentScale = parseInt(scaleControlValue.value, 10);
   if (currentScale < MAX_SCALE) {
     currentScale += SCALE_STEP;
-    scaleControlValue.value = `${currentScale}%`;
-    imgUploadPreview.style.transform = `scale(${currentScale / 100})`;
+    applyScale(currentScale);
   }
 });
 
 // Сброс масштаба при закрытии формы
 const resetScale = () => {
-  scaleControlValue.value = `${MAX_SCALE}%`;
-  imgUploadPreview.style.transform = `scale(${MAX_SCALE / 100})`;
+  applyScale(MAX_SCALE);
 };
 
 export {resetScale};
