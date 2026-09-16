c4a9a2f2f1728370116ad39bdb46674d8587331f
Keks <keks@htmlacademy.ru>
2026-09-16 17:38:35 +0300

    :heavy_check_mark: Сборка #13

---
+++ 13/js/main.js
@@ -0,0 +1,16 @@
+import {getData} from './api.js';
+import {renderPictures} from './pictures.js';
+import {openBigPicture} from './big-picture.js';
+import './form.js';
+import {showDataError, debounce} from './utils.js';
+import {showFilters, initFilters} from './filters.js';
+const onPhotosLoad = (photos) => {
+  renderPictures(photos, openBigPicture);
+  showFilters();
+  const renderFilteredPictures = debounce((filteredPictures) => {
+    renderPictures(filteredPictures, openBigPicture);
+  });
+
+  initFilters(photos, renderFilteredPictures);
+};
+getData(onPhotosLoad, showDataError);
